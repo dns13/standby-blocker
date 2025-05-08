4b7b85ded9cd3cee0f787467ce833b51e8c31114
Jan Schöppach <jan.schoeppach@bestsens.de>
2025-05-09 00:59:22 +0200

    Add option for token authentication

--- src/main.cpp
+++ src/main.cpp
@@ -16,13 +16,31 @@ auto unblock_standby() -> void {
 	spdlog::info("Standby unblocked");
 }
 
-
 auto print_output(bool blocked) -> std::string {
 	return fmt::format("{}", blocked ? "ENABLED" : "DISABLED");
 }
 
+auto check_auth_token(const crow::request& req, const std::string& auth_token) -> bool {
+	if (auth_token.empty()) {
+		return true;
+	}
+
+	const auto sent_header = req.get_header_value("Authorization");
+	if (sent_header.size() < 7) {
+		return false;
+	}
+
+	const auto sent_token = sent_header.substr(7); 
+
+	spdlog::debug("Authorization token: {}", sent_token);
+
+	return sent_token == auth_token;
+}
+
 auto main(int argc, char **argv) -> int {
 	unsigned int port = 8080u;
+	std::string auth_token{};
+	bool verbose = false;
 	
 	{
 		cxxopts::Options options(argv[0], "Standby Blocker");
@@ -31,6 +49,8 @@ auto main(int argc, char **argv) -> int {
 		options.add_options()
 			("h,help", "Print usage")
 			("p,port", "HTTP port", cxxopts::value<unsigned int>()->default_value(std::to_string(port)))
+			("t,token", "Authentication token", cxxopts::value<std::string>())
+			("v,verbose", "verbose output")
 		;
 		// clang-format on
 
@@ -44,27 +64,61 @@ auto main(int argc, char **argv) -> int {
 		if (result.count("port") != 0) {
 			port = result["port"].as<unsigned int>();
 		}
+
+		if (result.count("token") != 0) {
+			auth_token = result["token"].as<std::string>();
+		}
+
+		if (result.count("verbose") == 1u) {
+			spdlog::set_level(spdlog::level::debug);
+			spdlog::info("verbose output enabled");
+			verbose = true;
+		}
 	}
 
-	crow::SimpleApp app;
+	if (!auth_token.empty()) {
+		spdlog::info("Using authentication token: \"{}\"", auth_token);
+	} else {
+		spdlog::info("No authentication token provided, starting without authentication");
+	}
+
+	spdlog::info("Listening on 0.0.0.0:{}", port);
+
+	crow::SimpleApp app{};
+	app.loglevel(verbose ? crow::LogLevel::Debug : crow::LogLevel::Warning).port(port);
 
 	auto standby_blocked{false};
 
-	CROW_ROUTE(app, "/")([&standby_blocked](){
-		return print_output(standby_blocked);
+	CROW_ROUTE(app, "/")([&standby_blocked, auth_token](const crow::request& req){
+		if (!check_auth_token(req, auth_token)) {
+			spdlog::warn("Unauthorized access attempt to / endpoint from {}", req.remote_ip_address);
+			return crow::response(401, "Unauthorized");
+		}
+
+		return crow::response(200, print_output(standby_blocked));
 	});
 
-	CROW_ROUTE(app, "/enable")([&standby_blocked](){
+	CROW_ROUTE(app, "/enable")([&standby_blocked, auth_token](const crow::request& req){
+		if (!check_auth_token(req, auth_token)) {
+			spdlog::warn("Unauthorized access attempt to /enable endpoint from {}", req.remote_ip_address);
+			return crow::response(401, "Unauthorized");
+		}
+
 		block_standby();
 		standby_blocked = true;
-		return print_output(standby_blocked);
+		return crow::response(200, print_output(standby_blocked));
 	});
 
-	CROW_ROUTE(app, "/disable")([&standby_blocked](){
+	CROW_ROUTE(app, "/disable")([&standby_blocked, auth_token](const crow::request& req){
+		if (!check_auth_token(req, auth_token)) {
+			spdlog::warn("Unauthorized access attempt to /disable endpoint from {}", req.remote_ip_address);
+			return crow::response(401, "Unauthorized");
+		}
+
 		unblock_standby();
 		standby_blocked = false;
-		return print_output(standby_blocked);
+		return crow::response(200, print_output(standby_blocked));
 	});
 
-	app.port(port).run();
+	app.run();
 }
